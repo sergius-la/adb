aad098023e3273f658b32c88522692f0237f0e1a
Admin <sergius.la@gmail.com>
2019-07-09 07:44:35 -0700

    send

--- adb/user_actions.py
+++ adb/user_actions.py
@@ -3,7 +3,7 @@ from adb import ADB
 class UserActions:
 
     @staticmethod
-    def swipe(dev_id, x1, y1, x2 ,y2):
+    def swipe(dev_id: str, x1: int, y1: int, x2: int ,y2: int):
         """
         Method for perform swipe by coordinates
         """
@@ -12,10 +12,20 @@ class UserActions:
         ADB.exec_adb(command)
 
     @staticmethod
-    def tap(dev_id, x, y):
+    def tap(dev_id: str, x: int, y: int):
         """
         Method for perform tap by coordinates
         """
 
         command = "adb -s {dev_id} shell inout tap {x} {y}".format(dev_id=dev_id, x=x, y=y)
+        ADB.exec_adb(command)
+    
+    @staticmethod
+    def send_text(dev_id: str, text: str):
+        """
+        Method perform typing text
+        NOTE: Before use, make sure input text field in selected
+        """
+
+        command = "adb -s {dev_id} shell input text {text}".format(dev_id=dev_id, text=text.strip().replace(" ", "%s")) # %s - Space
         ADB.exec_adb(command)
