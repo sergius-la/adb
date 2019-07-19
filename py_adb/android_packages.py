3b56a56f068451cc053449629a260ad6e0209c90
Odin <sergius.la@gmail.com>
2019-07-18 19:04:54 -0700

    packages

--- py_adb/android_packages.py
+++ py_adb/android_packages.py
@@ -4,4 +4,18 @@ class AndroidKPackage(Enum):
     app = "App Name"
     package = "package"
     YOUTUBE = {app: "YouTube", package:"com.google.android.youtube"}
-    PLAYMARKET = {app: "Play Store", package:"com.android.vending"}
+    PLAY_STORE = {app: "Play Store", package:"com.android.vending"}
+
+class Package(object):
+    """
+    Package Info class
+    """
+
+    def __init__(self, package, appName, version):
+        self.appName = appName
+        self.version = version
+        self.package = package
+    
+    def __repr__(self):
+        return "{appName} - {version} - {package}".format(appName=self.appName, version=self.version, package=self.package)
+    
