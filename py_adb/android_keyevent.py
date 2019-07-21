c13b5cc84e90fb320094b532fbdf26fd6cbf690a
Odin <sergius.la@gmail.com>
2019-07-21 10:24:14 -0700

    is

--- py_adb/android_keyevent.py
+++ py_adb/android_keyevent.py
@@ -5,3 +5,4 @@ class AndroidKeyevent(Enum):
     key_code = "key_code"
     BACK = {command: "BACK", key_code:"4"}
     POWER_BUTTON = {command: "POWER BUTTON", key_code:"26"}
+    WAKEUP = {command: "Wake Up", key_code:"224"}
